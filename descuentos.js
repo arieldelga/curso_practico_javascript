0addd80a4dac39d30d32175f76a4bae93ca29fa5
Ariel <arieldelgadillo444@gmail.com>
2021-08-01 22:12:33 -0400

    taller de descuentos finalizado

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,8 @@
-// const preciOriginal = 120;
-// const descuento = 18;
+var cupones = [
+    {nombre : "cupon1", valor : 6},
+    {nombre : "cupon2", valor : 20},
+    {nombre : "cupon3", valor : 13}
+]
 
 
 function calcularPrecioConDescuento(precio, descuento) {
@@ -8,12 +11,52 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
 }
 
-// const porcentajePrecioConDescuento = 100 - descuento;
-// const precioConDescuento = (preciOriginal * porcentajePrecioConDescuento) / 100;
+function cuponExiste(cupon) {
+    let existe = false;
+    // cupones.forEach(function(cup) {
+    //     if (cupon == cup.name) {
+    //         existe = true;
+    //     }
+    // });
+    existe = cupones.some(function(cup) {
+        return cup.nombre == cupon;
+    });
+    return existe;
+}
+function seleccionarCupon(cupon) {
+    let cuponBuscado;
+    let valorDescuento = 0;
+    cuponBuscado = cupones.find(function(cup) {
+        return cup.nombre == cupon;
+    });
+    valorDescuento = cuponBuscado.valor;
+    return valorDescuento;
+}
+// function aplicarCupon(precio, cupon) {
+//     let precioConCupon = 0;
+//     const descuentoCupon = 0;
+//     if (cuponExiste(cupon)) {
+//         descuentoCupon = seleccionarCupon(cupon);
+//         precioConCupon = calcularPrecioConDescuento(precio, descuentoCupon);
+//     }
+//     return precioConCupon;
+// }
 
-// console.log({
-//     preciOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
-// });
+function calcularDescuentoButton() {
+    const inputPrice = document.getElementById("InputPrice");
+    const inputDiscount = document.getElementById("InputDiscount");
+    const inputCupon = document.getElementById("InputCupon");
+    const valuePrice = inputPrice.value;
+    const valueDiscount = inputDiscount.value;
+    const valueCupon = inputCupon.value;
+    let valueDiscountTotal = valueDiscount;
+
+    if (cuponExiste(valueCupon)){
+        valueDiscountTotal = Number(valueDiscount) + Number(seleccionarCupon(valueCupon));
+    }
+
+    let precioConDescuento = calcularPrecioConDescuento(valuePrice, valueDiscountTotal);
+
+    const resutltP = document.getElementById("ResultP");
+    resutltP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
